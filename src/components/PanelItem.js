8b8ca859512c87012f500db24d3dc47240e16792
trashpirate <nadinaz@sent.com>
2022-09-09 11:58:05 -0500

    adding About section

---
+++ src/components/PanelItem.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function PanelItem(props) {
+  return (
+    <>
+      <li className='panels__item'>
+        <div className='panels__item__card'>
+          <div className='panels__item__info'>
+            <h5 className='panels__item__text'>{props.text}</h5>
+          </div>
+          <div className='panels_number_field'>
+          <h1>{props.number}</h1>
+          </div>
+        </div>
+      </li>
+    </>
+  );
+}
+
+export default PanelItem;
